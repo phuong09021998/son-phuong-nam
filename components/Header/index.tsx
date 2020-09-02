566073886e92697e8ad27dac281e392758dd0814
Robert Nguyen <phuong09021998@gmail.com>
2020-09-02 11:07:13 +0700

    Finish header

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -8,8 +8,7 @@ const { Panel } = Collapse;
 
 interface MediaMatches {
   small: boolean;
-  medium: boolean;
-  large: boolean;
+  mediumLarge: boolean;
 }
 
 export default function Header() {
@@ -22,8 +21,58 @@ export default function Header() {
     </div>
   );
 
-  const renderMobile = () => (
-    <header className={styles.header}>
+  const renderMediumLargeDeviceLayout = () => (
+    <div className={styles.header}>
+      <Link href="/">
+        <div className={styles.logo}>
+          <div className={styles.logoWrapper}>
+            <img src="/icons/logo.svg" alt="logo" />
+          </div>
+          <div className={styles.text}>
+            <div className={styles.name}>PHƯƠNG NAM</div>
+            <div className={styles.slogan}>Thách Thức Thời Gian</div>
+          </div>
+        </div>
+      </Link>
+      <div className={styles.menuWrapper}>
+        <Link href="/">
+          <div className={styles.menuItem}>Trang Chủ</div>
+        </Link>
+        <Link href="/cua-hang">
+          <div className={styles.menuItem}>Cửa Hàng</div>
+        </Link>
+        <Link href="/dich-vu">
+          <div className={styles.menuItem}>Dịch Vụ</div>
+        </Link>
+        <Link href="/du-an">
+          <div className={styles.menuItem}>Dự Án</div>
+        </Link>
+        <Link href="/kien-thuc">
+          <div className={styles.menuItem}>Kiến Thức</div>
+        </Link>
+        <Link href="/gioi-thieu">
+          <div className={styles.menuItem}>Giới Thiệu</div>
+        </Link>
+        <Link href="/lien-he">
+          <div className={styles.menuItem}>Liên Hệ</div>
+        </Link>
+      </div>
+      <div className={styles.right}>
+        <div className={styles.cart}>
+          <img src="/icons/shopping-cart.svg" alt="cart" />
+        </div>
+        <div className={styles.loginRegister}>
+          <div className={styles.login}>ĐĂNG NHẬP</div>
+          <div className={styles.register}>
+            hoặc <span>đăng ký</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+
+  const renderSmallDeviceLayout = () => (
+    <div className={styles.header}>
       <div className={styles.top}>
         <Link href="/">
           <div className={styles.logo}>
@@ -75,22 +124,20 @@ export default function Header() {
           </Panel>
         </Collapse>
       </div>
-    </header>
+    </div>
   );
 
   return (
     <Media
       queries={{
         small: '(max-width: 768px)',
-        medium: '(min-width: 768px) and (max-width: 1200px)',
-        large: '(min-width: 1200px)',
+        mediumLarge: '(min-width: 769px)',
       }}
     >
       {(matches: MediaMatches) => (
         <Fragment>
-          {matches.small && renderMobile()}
-          {matches.medium && <p>I am medium!</p>}
-          {matches.large && <p>I am large!</p>}
+          {matches.small && renderSmallDeviceLayout()}
+          {matches.mediumLarge && renderMediumLargeDeviceLayout()}
         </Fragment>
       )}
     </Media>
--- layouts/Page/index.tsx
+++ layouts/Page/index.tsx
@@ -9,6 +9,10 @@ export default function PageLayout({ children }: any) {
       <Head>
         <title>Công Ty TNHH MTV Xây Dựng Sơn Phương Nam</title>
         <link rel="icon" href="/logo.svg" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&display=swap"
+          rel="stylesheet"
+        ></link>
       </Head>
       <Header />
       {children}
