0a43f07664c7687971f7c70ee0d2c6a041979590
Van Phuong Nguyen <phuong09021998@gmail.com>
2020-10-12 15:02:01 +0700

    Update seo tags

--- components/AboutUs/index.tsx
+++ components/AboutUs/index.tsx
@@ -18,7 +18,7 @@ export default function AboutUs() {
             <div className={styles.title}>Về chúng tôi</div>
             <div className={styles.question}>Chúng tôi là ai?</div>
             <div className={styles.text}>
-              Là doanh nghiệp dẫn đầu trong lĩnh vực thi công sơn epoxy, sơn nền nhà xưởng, chống thấm, ... Bán và phân phối các sản phẩm sơn epoxy Hàn quốc, Đài Loan. Chúng tôi luôn cam kết mang lại cho
+              Là doanh nghiệp dẫn đầu trong lĩnh vực thi công sơn epoxy, sơn nền nhà xưởng, chống thấm. Bán và phân phối các sản phẩm sơn epoxy Hàn quốc, Đài Loan. Chúng tôi luôn cam kết mang lại cho
               khách hàng sản phẩm sơn epoxy chất lượng tốt nhất với giá thành rẻ nhất giúp các doanh nghiệp công ty tiết
               kiệm triệt để chi phí đầu tư nhà xưởng ban đầu chính hãng giá rẻ ở Tp. Hồ Chí Minh...
             </div>
