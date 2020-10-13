96f6ab708307a1a45eb1b30b2c19c78ec3e8c5cd
Van Phuong Nguyen <phuong09021998@gmail.com>
2020-10-13 17:19:23 +0700

    Add build

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "eeb2e04f00a09af91b89";
+/******/ 	var hotCurrentHash = "42979db179075e1913b2";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
