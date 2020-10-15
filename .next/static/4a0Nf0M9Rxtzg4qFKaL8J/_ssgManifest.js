2b8f0134cd19026d1b54260c601612ef2e5350a4
Van Phuong Nguyen <phuong09021998@gmail.com>
2020-10-15 10:00:07 +0700

    Add build

---
+++ .next/static/4a0Nf0M9Rxtzg4qFKaL8J/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fgioi-thieu","\u002Flien-he","\u002F","\u002Fdich-vu","\u002Fdu-an","\u002Fkien-thuc","\u002Fcua-hang","\u002Fcua-hang\u002F[id]","\u002Fdich-vu\u002F[id]","\u002Fkien-thuc\u002F[id]","\u002Fdu-an\u002F[id]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
