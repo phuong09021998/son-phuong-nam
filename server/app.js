aa96d8c3db70189cac7311a3ad56e0818e42ab58
Van Phuong Nguyen <phuong09021998@gmail.com>
2020-10-16 13:09:31 +0700

    Fix cookie

--- server/app.js
+++ server/app.js
@@ -5,18 +5,18 @@ const cors = require('cors');
 const cookieParser = require('cookie-parser');
 require('dotenv').config();
 
+// Middleware
+express.use(cookieParser());
+express.use(bodyParser.json());
+express.use(morgan('dev'));
+express.use(cors());
+
 // Import routes
 const userRoutes = require('./routes/user');
 const postRoutes = require('./routes/post');
 const productRoutes = require('./routes/product');
 const siteRoutes = require('./routes/site');
 const messageRoutes = require('./routes/message');
-
-// Middleware
-express.use(bodyParser.json());
-express.use(cookieParser());
-express.use(morgan('dev'));
-express.use(cors());
 
 // Routes middlewares
 express.use('/api', userRoutes);
